a5b9dfa05819506d67ff485ea06ea016529329a6
Bruno Henrique <brunohenrique242@gmail.com>
2024-09-10 13:58:32 -0300

    Inciando o template

---
+++ app/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger mx1"></i></span>
+    <span>por Bruno Henrique</span>
+</footer>
+</body>
+
+</html>
