788e91d642405633824085b16a5b934994c1a205
Bruno Henrique <brunohenrique242@gmail.com>
2024-09-10 14:23:13 -0300

    Iniciando o grid-template

--- app/views/template/header.php
+++ app/views/template/header.php
@@ -10,17 +10,16 @@
     <link rel="stylesheet" href="assets/css/template.css">
     <title>In n` Out</title>
 </head>
-
-<header class="header">
-    <div class="logo">
-        <i class="icofont-travelling mr-2"></i>
-        <span class="font-weight-light">IN</span>
-        <span class="font-weight-bold mx-2">N`</span>
-        <span class="font-weight-light">OUT</span>
-        <i class="icofont-runner-alt-1 ml-2"></i>
-    </div>
-    <div class="menu-toggle mx-3"><i class="icofont-navigation-menu"></i></div>
-    <div class="sapacer"></div>
-</header>
-
+<!-- <body class="hide-sidebar"> -->
 <body>
+    <header class="header">
+        <div class="logo">
+            <i class="icofont-travelling mr-2"></i>
+            <span class="font-weight-light">IN</span>
+            <span class="font-weight-bold mx-2">N`</span>
+            <span class="font-weight-light">OUT</span>
+            <i class="icofont-runner-alt-1 ml-2"></i>
+        </div>
+        <div class="menu-toggle mx-3"><i class="icofont-navigation-menu"></i></div>
+        <div class="sapacer"></div>
+    </header>
