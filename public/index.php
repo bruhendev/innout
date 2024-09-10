b97997cd59fb06585af353955fa524e1ac7a9b06
Bruno Henrique <brunohenrique242@gmail.com>
2024-09-10 13:48:10 -0300

    Iniciando com a uri

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 
 require_once(dirname(__FILE__) . '/../app/config/config.php');
-require_once(CONTROLLER_PATH . '/login.php');
+// require_once(CONTROLLER_PATH . '/day_records.php');
 // loadView('login', ['texto' => 'abc123']);
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+if ($uri === '/' || $uri === '' || $uri === '/index.php') {
+    $uri = '/login.php';
+}
+
+// echo $uri;
+
+require_once(CONTROLLER_PATH . "/$uri");
