0a96552202e77971531fcce5ab0448c5f7713b75
Bruno Henrique <brunohenrique242@gmail.com>
2024-09-12 14:08:07 -0300

    implements metodo save in Model

--- app/config/database.php
+++ app/config/database.php
@@ -7,16 +7,28 @@ class Database
         $envPath = realpath(dirname(__FILE__) . '/../../env.ini');
         $env = parse_ini_file($envPath);
         $conn = new mysqli($env['host'], $env['username'], $env['password'], $env['database']);
-        if($conn->connect_error) {
+        if ($conn->connect_error) {
             die("Erro: " . $conn->connect_error);
         }
         return $conn;
     }
 
-    public static function getResultFromQuery($sql) {
+    public static function getResultFromQuery($sql)
+    {
         $conn = self::getConnection();
         $result = $conn->query($sql);
         $conn->close();
         return $result;
+    }
+
+    public static function executeSQL($sql)
+    {
+        $conn = self::getConnection();
+        if (!mysqli_query($conn, $sql)) {
+            throw new Exception(mysqli_error($conn));
+        }
+        $id = $conn->insert_id;
+        $conn->close();
+        return $id;
     }
 }
