566ba46c2e36613c47e37d948a23b14894dad9c6
Bruno Henrique <brunohenrique242@gmail.com>
2024-09-12 11:43:38 -0300

    iniciando autenticação

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if ($uri === '/' || $uri === '' || $uri === '/index.php') {
-    $uri = '/login.php';
+    $uri = '/day_records.php';
 }
 
 // echo $uri;
