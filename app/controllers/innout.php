f049eb3e677271c77e821ed27e7495a94fd9f2bb
Bruno Henrique <brunohenrique242@gmail.com>
2024-09-13 12:08:17 -0300

    implementado o simulador de ponto

--- app/controllers/innout.php
+++ app/controllers/innout.php
@@ -8,6 +8,9 @@ $user = $_SESSION['user'];
 $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 try {
     $currentTime = date('H:i:s');
+    if (isset($_POST['forcedTime'])) {
+        $currentTime = $_POST['forcedTime'];
+    }
     $records->innout($currentTime);
     addSuccessMsg("Ponto inserido com sucesso!");
 } catch (AppException $e) {
